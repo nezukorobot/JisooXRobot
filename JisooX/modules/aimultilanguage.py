46bb62986dfb1c0b640f32f5f97a067b65ff5ee4
nezukorobot <93088904+nezukorobot@users.noreply.github.com>
2021-12-02 09:28:03 +0530

    Update aimultilanguage.py

--- JisooX/modules/aimultilanguage.py
+++ JisooX/modules/aimultilanguage.py
@@ -3,7 +3,7 @@ import re
 import os
 import html
 import requests
-import SophiaBot.modules.sql.kuki_sql as sql
+import JisooX.modules.sql.kuki_sql as sql
 
 from time import sleep
 from telegram import ParseMode
